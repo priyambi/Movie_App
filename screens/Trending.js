b2f613d29613238a95fc36266137f0b7095a0ea6
priyambi <priyambi.hiran@gmail.com>
2021-09-24 22:43:42 +0530

    NEW CHANGE

--- screens/Trending.js
+++ screens/Trending.js
@@ -1,16 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import {
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-  StatusBar,
-  Text,
-} from 'react-native';
-export default function Trending(){
-    return(
-        <View>
-            <Text>Hello</Text>
-        </View>
+import React, {useEffect, useState} from 'react';
+import {FlatList, Text, View, StyleSheet, TouchableOpacity, Alert,Image} from 'react-native';
+
+
+export default function Upcoming() {
+  const [data, setData] = useState([]);
+  //console.log(data);
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/trending/all/day?api_key=575a360ec0e0b03cc0665d802b5198ce',
     )
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Trending Movies</Text>
+
+      <FlatList
+        data={data.results}
+        keyExtractor={({id}) => id}
+        renderItem={({item}) => 
+        <View >
+          <TouchableOpacity onPress={()=>Alert.alert('Release Date:  ' + item.release_date)}>
+            <Text>{item.title}</Text>
+            {/* <Text>{item.release_date}</Text> */}
+            
+          </TouchableOpacity>
+          </View>
+        }
+      />
+    </View>
+  );
 }
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lightblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+       
+      },
+      label:{
+        marginTop:70,
+        fontSize:30,
+        fontStyle:'italic',
+        textDecorationLine:'underline',
+        marginBottom:20,
+       
+      }
+})
--- screens/Upcoming.js
+++ screens/Upcoming.js
@@ -1,21 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import {
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-  StatusBar,
-  Text,
-} from 'react-native';
-export default function Upcoming(){
-    return(
-        <View style={styles.container}>
-            <Text>Hello</Text>
-        </View>
+import React, {useEffect, useState} from 'react';
+import {FlatList, Text, View, StyleSheet, TouchableOpacity, Alert,Image} from 'react-native';
+
+
+export default function Upcoming() {
+  const [data, setData] = useState([]);
+  //console.log(data);
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=575a360ec0e0b03cc0665d802b5198ce&language=en-US&page=1',
     )
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Upcoming Movies</Text>
+
+      <FlatList
+        data={data.results}
+        keyExtractor={({id}) => id}
+        renderItem={({item}) => 
+        <View >
+          <TouchableOpacity onPress={()=>Alert.alert('Release Date:  ' + item.release_date)}>
+            <Text>{item.title}</Text>
+            {/* <Text>{item.release_date}</Text> */}
+            
+          </TouchableOpacity>
+          </View>
+        }
+      />
+    </View>
+  );
 }
 const styles=StyleSheet.create({
-    container:{
-        height:"80%",
-    }
+    container: {
+        flex: 1,
+        backgroundColor: 'lightblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+       
+      },
+      label:{
+        marginTop:70,
+        fontSize:30,
+        fontStyle:'italic',
+        textDecorationLine:'underline',
+        marginBottom:20,
+       
+      }
 })
