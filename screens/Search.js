cee4232e6db46fa47b4c59245c3a44dea7f23273
priyambi <priyambi.hiran@gmail.com>
2021-10-01 10:29:34 +0530

    search screen edit

--- screens/Search.js
+++ screens/Search.js
@@ -3,14 +3,102 @@ import {
   View,
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
   Image,
+  TextInput,
   StatusBar,
   Text,
 } from 'react-native';
+import axios from 'axios';
+
+
 export default function Search(){
-    return(
-        <View>
-            <Text>Hello</Text>
-        </View>
-    )
+    
+    const apiurl='https://api.themoviedb.org/3/movie/upcoming?api_key=575a360ec0e0b03cc0665d802b5198ce&language=en-US&page=1';
+      
+     // const [data, setData] = useState([]);
+      const[state,setState]=useState({
+          s:"Enter a movie.......",
+          results:[],
+          selected:{}
+
+      });
+
+      const search=()=>{
+        axios(apiurl+"&s=" +state.s).then(({data}) => {
+            let results=data.results
+            console.log(results)
+            setState(prevState=>{return{...prevState,results:results}})
+        })
+      
+       
+          
+      }
+      return(
+          <View style={styles.container}>
+              <Text style={styles.title}>Movie Search</Text>
+              <TextInput 
+              style={styles.search} 
+              onChangeText={text=>setState(prevState=>{return{...prevState,s:text}})}
+              onSubmitEditing={search}
+              value={state.s}/>
+              <ScrollView style={styles.results}>
+                {state.results.map(results=>(
+                    <View key={results.id} style={styles.result}>
+                        <Text style={styles.heading}>{results.title}</Text>
+                    </View>
+                ))}
+              </ScrollView>
+          </View>
+      );
 }
+
+const  styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#223343',
+        alignItems:'center',
+        justifyContent:'flex-start',
+        paddingTop:40,
+        paddingHorizontal:20,
+
+
+    },
+    title:{
+        color:'#FFF',
+        fontSize:40,
+        fontWeight:"700",
+        alignSelf:'center',
+        marginBottom:20,
+        fontStyle: 'italic',
+        fontFamily:'cursive',
+
+    },
+    search:{
+        fontSize:15,
+        fontWeight:"100",
+        padding:10,
+        width:"100%",
+        height:50,
+        backgroundColor:'#FFF',
+        borderRadius:50,
+        marginBottom:40
+
+    },
+    results:{
+        flex:1,
+    },
+    result:{
+        flex:1,
+        width:'100%',
+        marginBottom:20,
+    },
+    heading:{
+        color:'#FFF',
+        fontSize:18,
+        fontWeight:'700',
+        padding:20,
+        backgroundColor:'#445565'
+    }
+
+})
--- screens/Upcoming.js
+++ screens/Upcoming.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, Text, View, StyleSheet, TouchableOpacity, Alert,Image,ImageBackground} from 'react-native';
-import Dialogue from '../screens/Dialogue';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View, StyleSheet, TouchableOpacity, Alert, Image, ImageBackground } from 'react-native';
+
+
 
 export default function Upcoming() {
   const [data, setData] = useState([]);
@@ -15,42 +16,42 @@ export default function Upcoming() {
   }, []);
   return (
     <ImageBackground
-    source={require('../assets/profile1.jpg')}
-    resizeMode="cover"
-    style={styles.bg}>
-      
+      source={require('../assets/profile1.jpg')}
+      resizeMode="cover"
+      style={styles.bg}>
+
 
       <FlatList
         data={data.results}
-        keyExtractor={({id}) => id}
-        renderItem={({item}) => 
-        <View style={{flex:1,flexDirection:'row',marginBottom:3}}>
-          {/* <TouchableOpacity onPress={()=>Alert.alert('Release Date:  ' + item.release_date)}> */}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) =>
+          <View style={{ flex: 1, flexDirection: 'row', marginBottom: 3 }}>
+            {/* <TouchableOpacity onPress={()=>Alert.alert('Release Date:  ' + item.release_date)}> */}
             {/* <Text>{item.title}</Text> */}
-            <Image onPress={()=>Alert.alert(item.overview)} style={{width:80,height:80,margin:5}} 
-              source={{ uri: 'https://image.tmdb.org/t/p/original/' + item.poster_path }}/>
-              {/* <Image
+            <Image onPress={() => Alert.alert(item.overview)} style={{ width: 80, height: 80, margin: 5 }}
+              source={{ uri: 'https://image.tmdb.org/t/p/original/' + item.poster_path }} />
+            {/* <Image
               source={{ uri:`https://image.tmdb.org/t/p/original/${item.poster_path}`}}/> */}
-              <View style={{flex:1,justifyContent:'center'}}>
-                <Text onPress={()=>Alert.alert(item.overview)} style={{fontSize:18,color:'black',marginBottom:15}}>{item.title}</Text>
-                <Text style={{fontSize:16,color:'black'}}>{item.release_date}</Text>
-              </View>
-            
-          {/* </TouchableOpacity> */}
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+              <Text onPress={() => Alert.alert(item.overview)} style={{ fontSize: 18, color: 'black', marginBottom: 15 }}>{item.title}</Text>
+              <Text style={{ fontSize: 16, color: 'black' }}>{item.release_date}</Text>
+            </View>
+
+            {/* </TouchableOpacity> */}
           </View>
         }
       />
     </ImageBackground>
   );
 }
-const styles=StyleSheet.create({
-    bg: {
-        flex: 1,
-        
-        
-     
-      
-       
-      },
-     
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+
+
+
+
+
+  },
+
 })
